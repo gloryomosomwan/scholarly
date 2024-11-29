8013ad93d621e5c271d2b0437c2911d15096810f
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2024-11-29 15:11:53 -0700

    Finished dashboard design

---
+++ app/index.tsx
@@ -0,0 +1,116 @@
+import { Text, StyleSheet, ScrollView, SafeAreaView, StatusBar, ActivityIndicator } from "react-native";
+import Animated from "react-native-reanimated";
+
+import { Card } from '@/components/Card'
+import { AnimatedView } from "react-native-reanimated/lib/typescript/component/View";
+
+export default function Index() {
+  const lecture = {
+    type: 'event',
+    topLeft: 'Lecture',
+    main: 'PROD 102',
+    bottomLeft: 'BSJ 3-20',
+    topRight: '9:00 AM - 10:00 AM',
+    bottomRight: '30 minutes left'
+  }
+
+  const exam = {
+    type: 'event',
+    topLeft: 'MARK 101',
+    main: 'Final Exam',
+    bottomLeft: 'ABC 1-234',
+    topRight: '12:00 PM - 3:00PM',
+  }
+
+  const task = {
+    type: 'task',
+    topLeft: "DEV 101",
+    main: "Review Reading 2",
+    topRight: "Due Today",
+    bottomRight: "Priority: Low"
+  }
+
+  return (
+    // <SafeAreaView>
+    <ScrollView style={styles.container} contentInsetAdjustmentBehavior="automatic">
+      <StatusBar
+        animated={true}
+        backgroundColor="#fff"
+      />
+      <Text style={styles.greeting}>Good morning, Glory.</Text>
+      <Text style={styles.date}>Friday, November 28</Text>
+      <Text style={styles.header}>Currently:</Text>
+      <Card
+        type={lecture.type}
+        topLeft={lecture.topLeft}
+        main={lecture.main}
+        bottomLeft={lecture.bottomLeft}
+        topRight={lecture.topRight}
+        bottomRight={lecture.bottomRight}
+      />
+      <Text style={styles.header}>Up Next:</Text>
+      <Card
+        type={lecture.type}
+        topLeft={lecture.topLeft}
+        main={lecture.main}
+        bottomLeft={lecture.bottomLeft}
+        topRight={lecture.topRight}
+        bottomRight={lecture.bottomRight}
+      />
+      <Text style={styles.header}>Today's Tasks:</Text>
+      <Card
+        type={task.type}
+        topLeft={task.topLeft}
+        main={task.main}
+        topRight={task.topRight}
+        bottomRight={task.bottomRight}
+      />
+      <Card
+        type={task.type}
+        topLeft={task.topLeft}
+        main={task.main}
+        topRight={task.topRight}
+        bottomRight={task.bottomRight}
+      />
+      <Card
+        type={task.type}
+        topLeft={task.topLeft}
+        main={task.main}
+        topRight={task.topRight}
+        bottomRight={task.bottomRight}
+      />
+      <Text style={styles.header}>Upcoming Dates:</Text>
+      <Card
+        type={exam.type}
+        topLeft={exam.topLeft}
+        main={exam.main}
+        bottomLeft={exam.bottomLeft}
+        topRight={exam.topRight}
+      />
+    </ScrollView>
+    // </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: '#f8faf9'
+  },
+  greeting: {
+    fontSize: 26,
+    marginBottom: 5,
+    fontWeight: '600'
+  },
+  date: {
+    fontSize: 20,
+    marginBottom: 10,
+    fontWeight: '600',
+    color: '#9496A1'
+  },
+  header: {
+    marginBottom: 10,
+    fontSize: 20,
+    fontWeight: '600'
+  }
+});
---
+++ components/Card.tsx
@@ -0,0 +1,74 @@
+import { StyleSheet, View, Text, ImageBackground } from "react-native"
+
+type CardProps = {
+  type: string,
+  topLeft?: string,
+  main: string,
+  bottomLeft?: string,
+  topRight?: string,
+  bottomRight?: string
+}
+
+// const image = require('../assets/card-backgrounds/img.jpg')
+
+export function Card(props: CardProps) {
+  return (
+    <View style={[styles.container, styles.shadowProp]}>
+      {/* <ImageBackground source={image} style={styles.image}> */}
+      <View style={styles.firstRow}>
+        <Text style={[styles.text]}>{props.topLeft}</Text>
+        <Text style={[styles.text]}>{props.topRight}</Text>
+      </View>
+      <View style={styles.secondRow}>
+        <Text style={[styles.text, props.type == 'event' ? styles.event : styles.task]}>{props.main}</Text>
+        <Text style={[styles.text]}>{props.bottomRight}</Text>
+      </View>
+      <View>
+        <Text style={[styles.text]}>{props.bottomLeft}</Text>
+      </View>
+      {/* </ ImageBackground> */}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  shadowProp: {
+    shadowColor: '#000000',
+    shadowOffset: { width: 3, height: 0 },
+    shadowOpacity: 0.24,
+    shadowRadius: 6,
+  },
+  container: {
+    backgroundColor: 'powderblue',
+    marginBottom: 10,
+    padding: 13,
+    borderRadius: 10,
+  },
+  text: {
+    color: "black"
+  },
+  firstRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  secondRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  event: {
+    fontSize: 22,
+    fontWeight: '600',
+  },
+  task: {
+    fontSize: 19,
+    fontWeight: '500'
+  },
+  // image: {
+  //   /* @info Make the image fill the containing view */
+  //   flex: 1,
+  //   /* @info Scale up the image to fill the container, preserving aspect ratio */
+  //   resizeMode: 'stretch',
+  //   justifyContent: 'center',
+  //   borderRadius: 10,
+  // },
+})
