98c4b0a86379b4f73430c6df976f2c336803b558
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2024-12-03 19:32:20 -0700

    Added some styling for schedule events

--- components/CourseEvent.tsx
+++ components/CourseEvent.tsx
@@ -11,16 +11,35 @@ type CourseEventProps = {
 export function CourseEvent(props: CourseEventProps) {
   return (
     <View style={styles.container}>
-      <Text>{props.eventType}</Text>
-      <Text>{props.time}</Text>
-      <Text>{props.frequency}</Text>
-      <Text>{props.location}</Text>
+      <Text style={styles.eventType}>{props.eventType}</Text>
+      <Text style={[styles.text, styles.time]}>{props.time}</Text>
+      <Text style={[styles.text, styles.frequency]}>{props.frequency}</Text>
+      <Text style={[styles.text, styles.location]}>{props.location}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginBottom: 20
+    marginBottom: 20,
   },
+  text: {
+    marginBottom: 2
+  },
+  eventType: {
+    fontWeight: '600',
+    fontSize: 17,
+    marginBottom: 3
+  },
+  time: {
+    fontSize: 16
+  },
+  frequency: {
+    fontSize: 15,
+    // color: 'grey'
+  },
+  location: {
+    color: 'grey',
+    fontSize: 14
+  }
 })
