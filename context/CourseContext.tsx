e052810d7ef7387f284fd463c5c69991642ce769
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-06-16 00:35:30 -0600

    Removed unused files

--- context/CourseContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-interface CurrentCourseContextType {
-  currentCourse: string | null;
-  setCurrentCourse: React.Dispatch<React.SetStateAction<string | null>>;
-}
-
-export const CurrentCourseContext = createContext<CurrentCourseContextType | null>(null);
