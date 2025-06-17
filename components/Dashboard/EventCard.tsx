60ff7b0df6fbac318f31f5ac632afba029392b92
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-06-16 18:01:54 -0600

    Made event type text more prominent

--- components/Dashboard/EventCard.tsx
+++ components/Dashboard/EventCard.tsx
@@ -108,7 +108,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: '500',
     color: 'white',
-    opacity: 0.7,
+    opacity: 0.9,
     marginBottom: 6,
   },
   courseContainer: {
