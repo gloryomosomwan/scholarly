4cb869a2202826008dda25b95051f34f8e71faa9
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-06-25 18:49:29 -0600

    Added icons to tab items

--- app/[courseCode]/_layout.tsx
+++ app/[courseCode]/_layout.tsx
@@ -3,7 +3,9 @@ import { withLayoutContext, useLocalSearchParams } from "expo-router";
 import { ParamListBase, TabNavigationState } from "@react-navigation/native";
 import { SafeAreaView, StyleSheet } from "react-native";
 
+import { useTheme } from "@/utils/useTheme";
 import Subheader from "@/components/CoursePage/Subheader";
+import { SymbolView } from "expo-symbols";
 
 const { Navigator } = createMaterialTopTabNavigator();
 
@@ -16,14 +18,29 @@ export const MaterialTopTabs = withLayoutContext<
 
 export default function TabLayout() {
   const { courseName, credits, grade } = useLocalSearchParams<{ courseName: string, credits: string, grade: string }>()
+  const theme = useTheme()
 
   return (
     <SafeAreaView style={styles.container}>
       <Subheader courseName={courseName} credits={credits} grade={grade} />
-      <MaterialTopTabs>
-        <MaterialTopTabs.Screen name="schedule" options={{ title: "Schedule" }} />
-        <MaterialTopTabs.Screen name="assignments" options={{ title: "Assignments" }} />
-        <MaterialTopTabs.Screen name="exams" options={{ title: "Exams" }} />
+      <MaterialTopTabs screenOptions={{
+        tabBarStyle: { backgroundColor: theme.secondary },
+        tabBarLabelStyle: { color: theme.text },
+        tabBarItemStyle: { flexDirection: 'row' },
+      }}>
+        <MaterialTopTabs.Screen name="schedule" options={{
+          title: "Schedule",
+          tabBarIcon: ({ color }) => <SymbolView size={20} name="calendar" tintColor={color} />,
+        }
+        } />
+        <MaterialTopTabs.Screen name="assignments" options={{
+          title: "Assignments",
+          tabBarIcon: ({ color }) => <SymbolView size={20} name="doc.text" tintColor={color} />,
+        }} />
+        <MaterialTopTabs.Screen name="exams" options={{
+          title: "Exams",
+          tabBarIcon: ({ color }) => <SymbolView size={20} name="graduationcap" tintColor={color} />,
+        }} />
       </MaterialTopTabs>
     </SafeAreaView>
   );
