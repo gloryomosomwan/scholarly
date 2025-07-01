9023039ada543273ff9c1e669c903f7699359d62
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-07-01 14:28:31 -0600

    File tree organization

---
+++ components/Menus/ActivityMenu.tsx
@@ -0,0 +1,38 @@
+import { SymbolView } from 'expo-symbols'
+
+import { DropdownMenuRoot, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuItem, DropdownMenuItemTitle, DropdownMenuItemIcon } from '@/components/Menus/Zeego'
+import { useTheme } from '../../utils/useTheme'
+
+export default function Menu() {
+  const theme = useTheme()
+  return (
+    <DropdownMenuRoot>
+      <DropdownMenuTrigger>
+        <SymbolView name={'ellipsis'} size={20} tintColor={theme.grey400} />
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent>
+        <DropdownMenuItem key="edit">
+          <DropdownMenuItemIcon ios={{
+            name: 'pencil',
+            pointSize: 20,
+            weight: 'semibold',
+            scale: 'medium',
+            paletteColors: [{ dark: theme.text, light: theme.text, }],
+          }} />
+          <DropdownMenuItemTitle>Edit Activity</DropdownMenuItemTitle>
+        </DropdownMenuItem>
+        <DropdownMenuItem key="delete">
+          <DropdownMenuItemTitle>Delete Activity</DropdownMenuItemTitle>
+          <DropdownMenuItemIcon ios={{
+            name: 'trash',
+            pointSize: 20,
+            weight: 'semibold',
+            scale: 'medium',
+            paletteColors: [{ dark: 'red', light: 'red', }],
+          }} />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenuRoot>
+  )
+}
