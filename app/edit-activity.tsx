5f19a17775cde02be87f92e4f732fb8ebd806b78
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-07-02 02:16:04 -0600

    Added buttons to edit activity

--- app/edit-activity.tsx
+++ app/edit-activity.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 import { useTheme } from '@/utils/useTheme'
@@ -8,26 +8,36 @@ export default function editActivity() {
   const theme = useTheme()
   return (
     <View style={[styles.container, { backgroundColor: theme.secondary }]}>
-      <TextInput
-        placeholder="Enter title"
-        style={[styles.titleInput, { color: theme.text }]}
-        placeholderTextColor={theme.grey500}
-      />
-      <View style={styles.detailRow}>
-        <SymbolView name={'calendar'} tintColor={theme.grey500} size={24} />
-        <Text style={[styles.detailText, { color: theme.grey500 }]}>Add due date</Text>
+      <View style={styles.formContainer}>
+        <TextInput
+          placeholder="Enter title"
+          style={[styles.titleInput, { color: theme.text }]}
+          placeholderTextColor={theme.grey500}
+        />
+        <View style={styles.detailRow}>
+          <SymbolView name={'calendar'} tintColor={theme.grey500} size={24} />
+          <Text style={[styles.detailText, { color: theme.grey500 }]}>Add due date</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <SymbolView name={'bookmark'} tintColor={theme.grey500} size={24} />
+          <Text style={[styles.detailText, { color: theme.grey500 }]}>Add course</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <SymbolView name={'flag'} tintColor={theme.grey500} size={24} />
+          <Text style={[styles.detailText, { color: theme.grey500 }]}>Add priority</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <SymbolView name={'note.text'} tintColor={theme.grey500} size={24} />
+          <Text style={[styles.detailText, { color: theme.grey500 }]}>Add notes</Text>
+        </View>
       </View>
-      <View style={styles.detailRow}>
-        <SymbolView name={'bookmark'} tintColor={theme.grey500} size={24} />
-        <Text style={[styles.detailText, { color: theme.grey500 }]}>Add course</Text>
-      </View>
-      <View style={styles.detailRow}>
-        <SymbolView name={'flag'} tintColor={theme.grey500} size={24} />
-        <Text style={[styles.detailText, { color: theme.grey500 }]}>Add priority</Text>
-      </View>
-      <View style={styles.detailRow}>
-        <SymbolView name={'note.text'} tintColor={theme.grey500} size={24} />
-        <Text style={[styles.detailText, { color: theme.grey500 }]}>Add notes</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity>
+          <Text style={[styles.buttonText, { color: theme.accent }]}>Mark completed</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={[styles.buttonText, { color: theme.dangerText }]}>Delete</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
@@ -37,8 +47,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 20,
-    paddingTop: 16,
-    gap: 24
+    paddingVertical: 16,
+    justifyContent: 'space-between',
+  },
+  formContainer: {
+    gap: 24,
   },
   titleInput: {
     fontSize: 30,
@@ -53,5 +66,15 @@ const styles = StyleSheet.create({
   detailText: {
     fontSize: 20,
     fontWeight: '500',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 40,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: '600',
   },
 })
