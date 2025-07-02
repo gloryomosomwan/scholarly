30642fba60f745d93047b5cb7d879b7efb473e5f
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-07-02 15:32:09 -0600

    Added DateTimePicker to modal

--- components/DateTimeModal.tsx
+++ components/DateTimeModal.tsx
@@ -1,6 +1,8 @@
-import React, { RefObject } from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React, { RefObject, useState } from 'react';
+import { Text, StyleSheet, View } from 'react-native';
 import { BottomSheetModal, BottomSheetView, BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
+
 import { useTheme } from '@/utils/useTheme'
 
 type DateTimeModalProps = {
@@ -10,19 +12,31 @@ type DateTimeModalProps = {
 
 export default function DateTimeModal({ bottomSheetModalRef, handleSheetChanges }: DateTimeModalProps) {
   const theme = useTheme()
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  };
+
   return (
     <BottomSheetModalProvider>
       <BottomSheetModal
         ref={bottomSheetModalRef}
         onChange={handleSheetChanges}
+        handleStyle={{ backgroundColor: theme.primary, borderRadius: 10 }}
+        handleIndicatorStyle={[styles.handleIndicator, {}]}
+        backgroundStyle={{ backgroundColor: theme.primary }}
       >
-        <BottomSheetView style={styles.contentContainer}>
-          <Text>Awesome 🎉</Text>
-          <Text>Awesome 🎉</Text>
-          <Text>Awesome 🎉</Text>
-          <Text>Awesome 🎉</Text>
-          <Text>Awesome 🎉</Text>
-          <Text>Awesome 🎉</Text>
+        <BottomSheetView style={[styles.contentContainer, { backgroundColor: theme.primary }]}>
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={'date'}
+            display={'inline'}
+            onChange={onChange}
+            accentColor={theme.accent}
+          />
         </BottomSheetView>
       </BottomSheetModal>
     </BottomSheetModalProvider>
@@ -33,5 +47,9 @@ const styles = StyleSheet.create({
   contentContainer: {
     flex: 1,
     alignItems: 'center',
+    paddingBottom: 20
+  },
+  handleIndicator: {
+    display: 'none'
   },
 })
