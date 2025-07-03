19bd72b2b6af0278fb4c5aef28c0c8951623f371
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-07-03 17:05:17 -0600

    Switched CourseSelectorModal to ScrollView; Added close button

--- components/Modals/CourseSelector/CourseSelectorModal.tsx
+++ components/Modals/CourseSelector/CourseSelectorModal.tsx
@@ -1,10 +1,11 @@
 import React, { RefObject, useState } from 'react';
 import { Text, StyleSheet, View } from 'react-native';
-import { BottomSheetModal, BottomSheetView, BottomSheetBackdrop } from '@gorhom/bottom-sheet';
+import { BottomSheetModal, BottomSheetScrollView, BottomSheetBackdrop } from '@gorhom/bottom-sheet';
 
 import { useTheme } from '@/utils/useTheme'
 import PressableOpacity from '@/components/PressableOpacity';
 import CourseItem from './CourseItem';
+import { SymbolView } from 'expo-symbols';
 
 type CourseSelectorProps = {
   bottomSheetModalRef: RefObject<BottomSheetModal>
@@ -15,8 +16,9 @@ export default function CourseSelectorModal({ bottomSheetModalRef }: CourseSelec
   return (
     <BottomSheetModal
       ref={bottomSheetModalRef}
-      snapPoints={[400]}
+      snapPoints={['45%']}
       enableDynamicSizing={false}
+      backgroundStyle={{ backgroundColor: theme.primary }}
       backdropComponent={props => (
         <BottomSheetBackdrop
           {...props}
@@ -26,11 +28,13 @@ export default function CourseSelectorModal({ bottomSheetModalRef }: CourseSelec
           pressBehavior="close"
         />
       )}
-      backgroundStyle={{ backgroundColor: theme.primary }}
     >
-      <BottomSheetView style={[styles.contentContainer, { backgroundColor: theme.primary }]}>
+      <PressableOpacity style={styles.closeButton} onPress={() => bottomSheetModalRef.current?.dismiss()}>
+        <SymbolView name={'xmark.circle.fill'} tintColor={theme.grey400} style={styles.closeButtonIcon} />
+      </PressableOpacity>
+      <BottomSheetScrollView contentContainerStyle={[styles.contentContainer, { backgroundColor: theme.primary }]}>
         <CourseItem code={'PHYS 102'} name={'Introduction to Physics'} color={'red'} />
-      </BottomSheetView>
+      </BottomSheetScrollView>
     </BottomSheetModal>
   )
 }
@@ -38,5 +42,11 @@ export default function CourseSelectorModal({ bottomSheetModalRef }: CourseSelec
 const styles = StyleSheet.create({
   contentContainer: {
     alignItems: 'center'
-  }
+  },
+  closeButton: {
+    alignSelf: 'flex-end',
+    right: 20,
+    marginBottom: 15
+  },
+  closeButtonIcon: {},
 })
