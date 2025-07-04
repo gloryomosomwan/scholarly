df12d91ae73be987d987ccafe4327c65715571b3
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-07-04 02:41:20 -0600

    Moved BottomSheetModalProvider back into EditActivity

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import { Stack } from "expo-router";
-import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 
 import { useTheme } from "@/hooks";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
@@ -9,24 +8,24 @@ export default function RootLayout() {
 
   return (
     <GestureHandlerRootView>
-      <BottomSheetModalProvider>
-        <Stack
-          screenOptions={{ contentStyle: { backgroundColor: theme.secondary } }}>
-          <Stack.Screen
-            name="edit-activity"
-            options={{
-              headerTitle: "",
-              headerBackTitle: 'Back',
-              headerStyle: { backgroundColor: theme.secondary },
-              headerTintColor: theme.accent,
-              headerShadowVisible: false
-            }}
-          />
-          <Stack.Screen
-            name="(tabs)"
-            options={{ headerShown: false }} />
-        </Stack>
-      </BottomSheetModalProvider>
+      <Stack
+        screenOptions={{ contentStyle: { backgroundColor: theme.secondary } }}>
+        <Stack.Screen
+          name="edit-activity"
+          options={{
+            // headerTitle: "",
+            // headerBackTitle: 'Back',
+            // headerStyle: { backgroundColor: theme.accent },
+            // headerTintColor: theme.accent,
+            // headerShadowVisible: false,
+            headerShown: false,
+            presentation: 'modal'
+          }}
+        />
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false }} />
+      </Stack>
     </GestureHandlerRootView>
   )
 }
--- app/edit-activity.tsx
+++ app/edit-activity.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useRef, useState } from 'react'
 import { StyleSheet, Text, View, TextInput, Keyboard } from 'react-native'
 import { SymbolView } from 'expo-symbols'
-import { BottomSheetModal } from '@gorhom/bottom-sheet'
+import { BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet'
 
 import { useTheme, usePriorityPalette } from '@/hooks'
 
@@ -44,131 +44,127 @@ export default function EditActivity() {
   }
 
   return (
-    <View style={[styles.container, { backgroundColor: theme.secondary }]}>
-      <View style={styles.formContainer}>
-
-        {/* Title */}
-        <TextInput
-          placeholder="Enter title"
-          style={[styles.titleInput, { color: theme.text }]}
-          placeholderTextColor={theme.grey500}
-          returnKeyType='done'
-          multiline
-          blurOnSubmit
-          value={title}
-          onChangeText={setTitle}
-        />
-
-        {/* Datetime */}
-        <PressableOpacity style={styles.detailRow} onPress={handlePresentModalPress}>
-          <SymbolView name={'calendar'} tintColor={theme.grey500} size={24} />
-          {
-            !addedDate
-              ? <Text style={[styles.detailText, { color: theme.grey500 }]}>Add date</Text>
-              : <Text style={[styles.detailText, { color: theme.grey500 }]}>{dueType === 'date' ? date.toLocaleDateString() : date.toLocaleString()}</Text>
-          }
-        </PressableOpacity>
-
-        {/* Course */}
-        <PressableOpacity onPress={() => {
-          Keyboard.dismiss()
-          courseSelectorModalRef.current?.present()
-        }}>
-          <View style={styles.detailRow}>
-            <SymbolView name={'bookmark'} tintColor={theme.grey500} size={24} />
-            {course ?
-              (() => {
-                const selected = courses.find(c => c.code === course)
-                return (
-                  <View style={[styles.courseTag, { backgroundColor: theme.grey100 }]}>
-                    <View style={[styles.courseDot, { backgroundColor: selected?.color ?? 'grey' }]} />
-                    <Text style={[styles.courseText, { color: theme.text }]}>{course}</Text>
-                  </View>
-                )
-              })()
-              :
-              <Text style={[styles.detailText, { color: theme.grey500 }]}>Add course</Text>
-            }
-          </View>
-        </PressableOpacity>
-
-        {/* Priority */}
-        <PressableOpacity onPress={() => {
-          Keyboard.dismiss()
-          prioritySelectorModalRef.current?.present()
-        }}>
-          <View style={styles.detailRow}>
-            <SymbolView name={'flag'} tintColor={theme.grey500} size={24} />
-            {priority ?
-              (() => {
-                return (
-                  <View style={[styles.priorityTag, { backgroundColor: priorityPalette.backgroundColor }]}>
-                    <Text style={[styles.priorityText, { color: priorityPalette.color }]}>
-                      {priority.toUpperCase()}
-                    </Text>
-                  </View>
-                )
-              })()
-              :
-              <Text style={[styles.detailText, { color: theme.grey500 }]}>Add priority</Text>
-            }
-          </View>
-        </PressableOpacity>
-
-        {/* Notes */}
-        <View style={styles.detailRow}>
-          <SymbolView name={'note.text'} tintColor={theme.grey500} size={24} />
+    <BottomSheetModalProvider>
+      <View style={[styles.container, { backgroundColor: theme.secondary }]}>
+        <View style={styles.formContainer}>
+          {/* Title */}
           <TextInput
-            placeholder="Add notes"
-            style={[styles.detailText, { color: theme.text, flex: 1 }]}
+            placeholder="Enter title"
+            style={[styles.titleInput, { color: theme.text }]}
             placeholderTextColor={theme.grey500}
-            multiline
             returnKeyType='done'
+            multiline
             blurOnSubmit
-            value={notes}
-            onChangeText={setNotes}
+            value={title}
+            onChangeText={setTitle}
           />
+          {/* Datetime */}
+          <PressableOpacity style={styles.detailRow} onPress={handlePresentModalPress}>
+            <SymbolView name={'calendar'} tintColor={theme.grey500} size={24} />
+            {
+              !addedDate
+                ? <Text style={[styles.detailText, { color: theme.grey500 }]}>Add date</Text>
+                : <Text style={[styles.detailText, { color: theme.grey500 }]}>{dueType === 'date' ? date.toLocaleDateString() : date.toLocaleString()}</Text>
+            }
+          </PressableOpacity>
+          {/* Course */}
+          <PressableOpacity onPress={() => {
+            Keyboard.dismiss()
+            courseSelectorModalRef.current?.present()
+          }}>
+            <View style={styles.detailRow}>
+              <SymbolView name={'bookmark'} tintColor={theme.grey500} size={24} />
+              {course ?
+                (() => {
+                  const selected = courses.find(c => c.code === course)
+                  return (
+                    <View style={[styles.courseTag, { backgroundColor: theme.grey100 }]}>
+                      <View style={[styles.courseDot, { backgroundColor: selected?.color ?? 'grey' }]} />
+                      <Text style={[styles.courseText, { color: theme.text }]}>{course}</Text>
+                    </View>
+                  )
+                })()
+                :
+                <Text style={[styles.detailText, { color: theme.grey500 }]}>Add course</Text>
+              }
+            </View>
+          </PressableOpacity>
+          {/* Priority */}
+          <PressableOpacity onPress={() => {
+            Keyboard.dismiss()
+            prioritySelectorModalRef.current?.present()
+          }}>
+            <View style={styles.detailRow}>
+              <SymbolView name={'flag'} tintColor={theme.grey500} size={24} />
+              {priority ?
+                (() => {
+                  return (
+                    <View style={[styles.priorityTag, { backgroundColor: priorityPalette.backgroundColor }]}>
+                      <Text style={[styles.priorityText, { color: priorityPalette.color }]}>
+                        {priority.toUpperCase()}
+                      </Text>
+                    </View>
+                  )
+                })()
+                :
+                <Text style={[styles.detailText, { color: theme.grey500 }]}>Add priority</Text>
+              }
+            </View>
+          </PressableOpacity>
+          {/* Notes */}
+          <View style={styles.detailRow}>
+            <SymbolView name={'note.text'} tintColor={theme.grey500} size={24} />
+            <TextInput
+              placeholder="Add notes"
+              style={[styles.detailText, { color: theme.text, flex: 1 }]}
+              placeholderTextColor={theme.grey500}
+              multiline
+              returnKeyType='done'
+              blurOnSubmit
+              value={notes}
+              onChangeText={setNotes}
+            />
+          </View>
         </View>
-      </View>
-      <View style={[styles.buttonContainer, {}]}>
-        <PressableOpacity>
-          <Text style={[styles.buttonText, { color: theme.accent }]}>Mark completed</Text>
-        </PressableOpacity>
-        <PressableOpacity>
-          <Text style={[styles.buttonText, { color: theme.dangerText }]}>Delete</Text>
-        </PressableOpacity>
-      </View>
-
-      <DateTimeModal initialDate={date} handleSetDate={handleSetDate} bottomSheetModalRef={bottomSheetModalRef} handleSheetChanges={handleSheetChanges} />
-      <CustomBottomSheetModal bottomSheetModalRef={courseSelectorModalRef} scrollable>
-        {courses.map(course => (
-          <CourseItem
-            key={course.code}
-            code={course.code}
-            name={course.name ?? ''}
-            color={course.color}
-            onSelect={({ code }) => {
-              setCourse(code)
-              courseSelectorModalRef.current?.dismiss()
-            }}
-          />
-        ))}
-      </CustomBottomSheetModal>
-      <CustomBottomSheetModal bottomSheetModalRef={prioritySelectorModalRef} showHandle={false}>
-        <PriorityItem level={'high'} onSelect={() => {
-          setPriority('high')
-          prioritySelectorModalRef.current?.dismiss()
-        }} />
-        <PriorityItem level={'medium'} onSelect={() => {
-          setPriority('medium')
-          prioritySelectorModalRef.current?.dismiss()
-        }} />
-        <PriorityItem level={'low'} onSelect={() => {
-          setPriority('low')
-          prioritySelectorModalRef.current?.dismiss()
-        }} />
-      </CustomBottomSheetModal>
-    </View >
+        <View style={[styles.buttonContainer, {}]}>
+          <PressableOpacity>
+            <Text style={[styles.buttonText, { color: theme.accent }]}>Mark completed</Text>
+          </PressableOpacity>
+          <PressableOpacity>
+            <Text style={[styles.buttonText, { color: theme.dangerText }]}>Delete</Text>
+          </PressableOpacity>
+        </View>
+        <DateTimeModal initialDate={date} handleSetDate={handleSetDate} bottomSheetModalRef={bottomSheetModalRef} handleSheetChanges={handleSheetChanges} />
+        <CustomBottomSheetModal bottomSheetModalRef={courseSelectorModalRef} scrollable>
+          {courses.map(course => (
+            <CourseItem
+              key={course.code}
+              code={course.code}
+              name={course.name ?? ''}
+              color={course.color}
+              onSelect={({ code }) => {
+                setCourse(code)
+                courseSelectorModalRef.current?.dismiss()
+              }}
+            />
+          ))}
+        </CustomBottomSheetModal>
+        <CustomBottomSheetModal bottomSheetModalRef={prioritySelectorModalRef} showHandle={false}>
+          <PriorityItem level={'high'} onSelect={() => {
+            setPriority('high')
+            prioritySelectorModalRef.current?.dismiss()
+          }} />
+          <PriorityItem level={'medium'} onSelect={() => {
+            setPriority('medium')
+            prioritySelectorModalRef.current?.dismiss()
+          }} />
+          <PriorityItem level={'low'} onSelect={() => {
+            setPriority('low')
+            prioritySelectorModalRef.current?.dismiss()
+          }} />
+        </CustomBottomSheetModal>
+      </View >
+    </BottomSheetModalProvider>
   )
 }
 
