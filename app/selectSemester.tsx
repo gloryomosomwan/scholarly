a6e749674769b297499c1dc6f1124aa96d0c4ee7
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-07-04 16:46:17 -0600

    Forgot to add file

---
+++ app/selectSemester.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function selectSemester() {
+  return (
+    <View>
+      <Text>selectSemester</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
