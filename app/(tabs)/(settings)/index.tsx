525dfa1e8b3fe803a6d01c55dbf400dd1ce9f39a
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-07-07 12:16:51 -0600

    Forgot to add file

---
+++ app/(tabs)/(settings)/index.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, ScrollView } from 'react-native';
+
+import SettingsGroup from '@/components/SettingsPage/SettingsGroup';
+import SettingsItem from '@/components/SettingsPage/SettingsItem';
+
+export default function Tab() {
+  return (
+    <ScrollView style={[styles.container, {}]} contentInsetAdjustmentBehavior='automatic' >
+      <SettingsGroup title={'GENERAL'}>
+        <SettingsItem title={'A setting!'} />
+        <SettingsItem title={'A setting!'} />
+        <SettingsItem title={'A setting!'} />
+      </SettingsGroup>
+
+    </ScrollView >
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    paddingVertical: 20
+  },
+});
