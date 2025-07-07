036fff5e9755060f7911002d65f19acaf6888bed
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-07-06 21:43:56 -0600

    Changed courses page icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -39,7 +39,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="(courses)"
         options={{
-          tabBarIcon: ({ color }) => <SymbolView size={35} name="book" tintColor={color} />,
+          tabBarIcon: ({ color }) => <SymbolView size={35} name="graduationcap.fill" tintColor={color} />,
         }}
       />
       <Tabs.Screen
