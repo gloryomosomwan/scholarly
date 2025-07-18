8c2748145e2daafb23a93740a9968b4ffac316a7
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-07-18 00:55:42 -0600

    Forgot to add file

---
+++ db/init.ts
@@ -0,0 +1,5 @@
+import { drizzle } from 'drizzle-orm/expo-sqlite'
+import { openDatabaseSync } from 'expo-sqlite'
+
+export const sqlite = openDatabaseSync('databaseName', { enableChangeListener: true })
+export const db = drizzle(sqlite)
