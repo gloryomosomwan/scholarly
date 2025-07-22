7eb294b9799cff0786d8cb8310c721c8310b8553
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-07-21 23:19:30 -0600

    Added logging to Drizzle instance

--- db/init.ts
+++ db/init.ts
@@ -2,4 +2,4 @@ import { drizzle } from 'drizzle-orm/expo-sqlite'
 import { openDatabaseSync } from 'expo-sqlite'
 
 export const sqlite = openDatabaseSync('databaseName', { enableChangeListener: true })
-export const db = drizzle(sqlite)
+export const db = drizzle(sqlite, { logger: true })
