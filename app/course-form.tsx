7cda249845461e4c83f72faf39c0dbaaadddf15e
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-08-05 17:21:05 -0600

    Remove unused styles and code in course-form.tsx

--- app/course-form.tsx
+++ app/course-form.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React, { useState } from 'react'
 import { useRouter } from 'expo-router'
 
 import PrimaryTextInputField from '@/components/Form/PrimaryTextInputField'
 import TextInputField from '@/components/Form/TextInputField'
 import ColorPicker from '@/components/Form/ColorPicker'
+import ButtonRow from '@/components/Form/ButtonRow'
 
 import { useTheme } from '@/hooks'
 
@@ -21,29 +22,8 @@ export default function CourseForm() {
         <PrimaryTextInputField placeholder='Enter course code' value={courseCode} onChangeText={setCourseCode} />
         <TextInputField placeholder="Enter course name" value={courseName} onChangeText={setCourseName} />
         <ColorPicker selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
-
       </View>
-      <View style={styles.buttonRowContainer}>
-        <Pressable
-          style={[styles.closeButton, { backgroundColor: '#eee' }]}
-          onPress={() => {
-            router.back()
-          }}
-          accessibilityLabel="Close"
-        >
-          <Text style={[styles.closeButtonText, { color: '#333' }]}> × </Text>
-        </Pressable>
-        <Pressable
-          style={[styles.saveButton, { backgroundColor: '#007AFF' }]}
-          onPress={() => {
-            console.log(courseCode, courseName, selectedColor)
-            router.back()
-          }}
-          accessibilityLabel="Save"
-        >
-          <Text style={[styles.saveButtonText, { color: '#fff' }]}> Save </Text>
-        </Pressable>
-      </View>
+      {/* <ButtonRow /> */}
     </View>
   )
 }
@@ -61,42 +41,5 @@ const styles = StyleSheet.create({
   fieldContainer: {
     flexDirection: 'row',
     gap: 16
-  },
-
-  buttonRowContainer: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    marginTop: 32,
-    paddingHorizontal: 20,
-    paddingBottom: 40
-  },
-  closeButton: {
-    width: 44,
-    height: 44,
-    borderRadius: 22,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 16,
-    elevation: 2,
-  },
-  closeButtonText: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    lineHeight: 28,
-  },
-  saveButton: {
-    paddingHorizontal: 20,
-    height: 44,
-    borderRadius: 22,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginLeft: 16,
-    elevation: 2,
-  },
-  saveButtonText: {
-    fontSize: 18,
-    fontWeight: '600',
-    letterSpacing: 1,
   },
 })
