4b8f0b21003a07b3cb628eb08698021cd2e015a3
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-08-05 19:23:24 -0600

    Add dueType state to test-form.tsx

--- app/test-form.tsx
+++ app/test-form.tsx
@@ -8,20 +8,22 @@ import TextInputField from '@/components/Form/TextInputField'
 import ButtonRow from '@/components/Form/ButtonRow'
 
 import { useTheme } from '@/hooks'
+import { DueType } from '@/types/types'
 
 export default function EventForm() {
   const theme = useTheme()
   const [title, setTitle] = useState('Add title')
   const [start, setStart] = useState<Date | null>(null)
   const [end, setEnd] = useState<Date | null>(null)
+  const [dueType, setDueType] = useState<DueType | null>(null)
   const [location, setLocation] = useState('')
   return (
     <BottomSheetModalProvider>
       <View style={[styles.container, {}]}>
         <View style={[styles.formContainer, {}]}>
           <PrimaryTextInputField placeholder='Add name' value={title} onChangeText={setTitle} />
-          <DateTimePicker placeholder='Add start date' date={start} setDate={setStart} dueType={'date'} />
-          <DateTimePicker placeholder='Add end date' date={end} setDate={setEnd} dueType={'date'} />
+          <DateTimePicker placeholder='Add start date' date={start} setDate={setStart} dueType={dueType} setDueType={setDueType} />
+          <DateTimePicker placeholder='Add end date' date={end} setDate={setEnd} dueType={dueType} setDueType={setDueType} />
           <TextInputField placeholder='Add location' value={location} onChangeText={setLocation} />
         </View>
         {/* <ButtonRow /> */}
