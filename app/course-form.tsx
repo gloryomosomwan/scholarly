75090efc42968a6963183cb09b0d6f8334081008
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-08-05 20:53:46 -0600

    [course-form.tsx] Changed field types and added create function

--- app/course-form.tsx
+++ app/course-form.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet, View } from 'react-native'
 import React, { useState } from 'react'
+import { router } from 'expo-router'
 
 import PrimaryTextInputField from '@/components/Form/PrimaryTextInputField'
 import TextInputField from '@/components/Form/TextInputField'
@@ -7,27 +8,46 @@ import ColorPicker from '@/components/Form/ColorPicker'
 import ButtonRow from '@/components/Form/ButtonRow'
 
 import { useTheme } from '@/hooks'
+import { db } from '@/db/init'
+import { courses } from '@/db/schema'
+import { Button } from 'react-native'
 
 export default function CourseForm() {
   const theme = useTheme()
-  const [courseCode, setCourseCode] = useState('')
-  const [courseName, setCourseName] = useState('')
-  const [selectedColor, setSelectedColor] = useState<string | null>(null);
-  const [instrutor, setInstructor] = useState('')
-  const [lectureSchedule, setLectureSchedule] = useState('')
-  const [seminarSchedule, setSeminarSchedule] = useState('')
-  const [labSchedule, setLabSchedule] = useState('')
+  const [code, setCode] = useState<string | null>(null)
+  const [name, setName] = useState<string | null>(null)
+  const [color, setColor] = useState<string | null>(null);
+  const [instructor, setInstructor] = useState<string | null>(null)
+  const [lectureSchedule, setLectureSchedule] = useState<string | null>(null)
+  const [seminarSchedule, setSeminarSchedule] = useState<string | null>(null)
+  const [labSchedule, setLabSchedule] = useState<string | null>(null)
+
+  const createCourse = async () => {
+    if (code !== null && name !== null && color !== null) {
+      await db.insert(courses).values({
+        name: name,
+        code: code,
+        color: color,
+        instructor: instructor,
+        lectureSchedule: lectureSchedule,
+        seminarSchedule: seminarSchedule,
+        labSchedule: labSchedule
+      })
+      router.back()
+    }
+  }
 
   return (
     <View style={[styles.container, {}]}>
       <View style={[styles.formContainer, {}]}>
-        <PrimaryTextInputField placeholder='Add course code' value={courseCode} onChangeText={setCourseCode} />
-        <TextInputField icon='graduationcap' placeholder="Add course name" value={courseName} onChangeText={setCourseName} />
-        <TextInputField icon='person' placeholder="Add instructor name" value={instrutor} onChangeText={setInstructor} />
+        <PrimaryTextInputField placeholder='Add course code' value={code} onChangeText={setCode} />
+        <TextInputField icon='graduationcap' placeholder="Add course name" value={name} onChangeText={setName} />
+        <TextInputField icon='person' placeholder="Add instructor name" value={instructor} onChangeText={setInstructor} />
         <TextInputField icon='book.closed' placeholder="Add lecture schedule" value={lectureSchedule} onChangeText={setLectureSchedule} />
         <TextInputField icon='flask' placeholder="Add lab schedule" value={labSchedule} onChangeText={setLabSchedule} />
         <TextInputField icon='bubble.left.and.bubble.right' placeholder="Add seminar schedule" value={seminarSchedule} onChangeText={setSeminarSchedule} />
-        <ColorPicker selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <ColorPicker selectedColor={color} setSelectedColor={setColor} />
+        <Button title='Create' onPress={createCourse} />
       </View>
       {/* <ButtonRow /> */}
     </View>
