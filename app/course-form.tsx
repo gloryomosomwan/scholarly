a135df56e8927d0efb4ab7d07603bab10bfa3d20
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-08-05 20:24:19 -0600

    Add all course fields to form

--- app/course-form.tsx
+++ app/course-form.tsx
@@ -13,12 +13,20 @@ export default function CourseForm() {
   const [courseCode, setCourseCode] = useState('')
   const [courseName, setCourseName] = useState('')
   const [selectedColor, setSelectedColor] = useState<string | null>(null);
+  const [instrutor, setInstructor] = useState('')
+  const [lectureSchedule, setLectureSchedule] = useState('')
+  const [seminarSchedule, setSeminarSchedule] = useState('')
+  const [labSchedule, setLabSchedule] = useState('')
 
   return (
     <View style={[styles.container, {}]}>
       <View style={[styles.formContainer, {}]}>
-        <PrimaryTextInputField placeholder='Enter course code' value={courseCode} onChangeText={setCourseCode} />
-        <TextInputField placeholder="Enter course name" value={courseName} onChangeText={setCourseName} />
+        <PrimaryTextInputField placeholder='Add course code' value={courseCode} onChangeText={setCourseCode} />
+        <TextInputField icon='graduationcap' placeholder="Add course name" value={courseName} onChangeText={setCourseName} />
+        <TextInputField icon='person' placeholder="Add instructor name" value={instrutor} onChangeText={setInstructor} />
+        <TextInputField icon='book.closed' placeholder="Add lecture schedule" value={lectureSchedule} onChangeText={setLectureSchedule} />
+        <TextInputField icon='flask' placeholder="Add lab schedule" value={labSchedule} onChangeText={setLabSchedule} />
+        <TextInputField icon='bubble.left.and.bubble.right' placeholder="Add seminar schedule" value={seminarSchedule} onChangeText={setSeminarSchedule} />
         <ColorPicker selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
       </View>
       {/* <ButtonRow /> */}
