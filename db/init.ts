e5ed8c1d6bfa9fa4d3b4645b22d1f296f6f06504
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-08-06 19:25:36 -0600

    Add statement to init.ts

--- db/init.ts
+++ db/init.ts
@@ -1,5 +1,9 @@
 import { drizzle } from 'drizzle-orm/expo-sqlite'
 import { openDatabaseSync } from 'expo-sqlite'
+import { courses, tasks } from './schema'
 
 export const sqlite = openDatabaseSync('databaseName', { enableChangeListener: true })
-export const db = drizzle(sqlite, { logger: false })
+export const db = drizzle(sqlite, { logger: true })
+// db.select().from(courses).limit(1).then((d) => {
+//   console.log(d)
+// })
