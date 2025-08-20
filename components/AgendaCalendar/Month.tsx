31e1f58bee6ca6d020e7f6f4249d57f8a85eba6d
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-08-19 19:23:37 -0600

    style(Month): remove unused code

--- components/AgendaCalendar/Month.tsx
+++ components/AgendaCalendar/Month.tsx
@@ -23,10 +23,6 @@ function chunk<T>(array: T[], size: number): T[][] {
 }
 
 export default function Month({ initialDay, selectedDatePosition, setCalendarBottom }: MonthProps) {
-  // const dates = getDates(initialDay)
-  // const paddedDates = padDatesArray(dates)
-  // const daysArray = createDays(paddedDates, initialDay, selectedDatePosition)
-  // const weeks = createWeeks(daysArray)
   const insets = useSafeAreaInsets()
   let paddingTop = 0;
   if (Platform.OS === 'android') {
@@ -60,10 +56,7 @@ export default function Month({ initialDay, selectedDatePosition, setCalendarBot
     const numDaysInMonth = getDaysInMonth(initialDay)
     let firstOfMonth = startOfMonth(initialDay)
     let rawDates: Date[] = []
-    // let currentDay = firstDay
     for (let i = 0; i < numDaysInMonth; i++) {
-      // rawDates.push(currentDay)
-      // currentDay = addDays(currentDay, 1)
       rawDates.push(addDays(firstOfMonth, i))
     }
 
@@ -116,64 +109,6 @@ export default function Month({ initialDay, selectedDatePosition, setCalendarBot
     </View>
   )
 }
-
-// function getDates(initialDay: Date) {
-//   const numDaysInMonth = getDaysInMonth(initialDay)
-//   let firstDay = startOfMonth(initialDay)
-//   let dates = []
-//   let currentDay = firstDay
-//   for (let i = 0; i < numDaysInMonth; i++) {
-//     dates.push(currentDay)
-//     currentDay = addDays(currentDay, 1)
-//   }
-//   return dates
-// }
-
-// function createDays(dates: Date[], initialDay: Date, selectedDatePosition: SharedValue<number>) {
-//   let days: JSX.Element[] = []
-//   dates.map((date) => {
-//     days.push(
-//       <Day key={date.toDateString()}
-//         date={date}
-//         firstDayOfMonth={initialDay}
-//         selectedDatePosition={selectedDatePosition}
-//         dayType='month'
-//       />
-//     )
-//   })
-//   return days
-// }
-
-// function padDatesArray(dates: Date[]) {
-//   let firstDay = dates[0]
-//   let dayOfFirstDay = getDay(dates[0])
-//   let i = dayOfFirstDay
-//   let currentDay = firstDay
-//   while (i > 0) {
-//     currentDay = subDays(currentDay, 1)
-//     dates.unshift(currentDay)
-//     i--
-//   }
-//   let lastDay = dates[dates.length - 1]
-//   while (dates.length < 42) {
-//     lastDay = addDays(lastDay, 1)
-//     dates.push(lastDay)
-//   }
-//   return dates
-// }
-
-// function createWeeks(daysArray: React.ReactNode[]) {
-//   let weeks = []
-//   for (let i = 0; i < 6; i++) {
-//     let week = []
-//     for (let j = 0; j < 7; j++) {
-//       let day = daysArray.shift()
-//       week.push(day)
-//     }
-//     weeks.push(week)
-//   }
-//   return weeks
-// }
 
 const styles = StyleSheet.create({
   container: {
