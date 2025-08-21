e6c50062a48fde1038e2e1285bbf0e9e4a849451
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-08-20 20:21:29 -0600

    style(useDatabase): organize hooks

--- hooks/useDatabase.ts
+++ hooks/useDatabase.ts
@@ -2,7 +2,7 @@ import { useLiveQuery } from "drizzle-orm/expo-sqlite";
 import { and, eq, getTableColumns, gte, like, lte } from 'drizzle-orm';
 import { useSQLiteContext } from "expo-sqlite";
 import { AnySQLiteTable } from "drizzle-orm/sqlite-core";
-import { endOfDay, endOfWeek, lastDayOfMonth, startOfDay, startOfWeek } from "date-fns";
+import { endOfDay, startOfDay } from "date-fns";
 
 import { assignments, courses, events, semesters, tasks } from '@/db/schema';
 import { db } from '@/db/init';
@@ -11,6 +11,143 @@ import { Course, Semester } from "@/types";
 import { useUserStore } from "@/stores";
 import { rawAssignment, rawCourse, rawTask } from "@/types/drizzle";
 
+// Assignments
+export function useAssignments() {
+  const { data } = useLiveQuery(db.select().from(assignments))
+  const assignmentData = data.map(convertRawAssignment)
+  return assignmentData
+}
+
+export function useAssignmentsByDateRange(firstDay: Date, lastDay: Date) {
+  const { data } = useLiveQuery(db.select().from(assignments).where(
+    and(
+      gte(assignments.due, startOfDay(firstDay).toISOString()),
+      lte(assignments.due, endOfDay(lastDay).toISOString())
+    )
+  ))
+  const assignmentData = data.map(convertRawAssignment)
+  return assignmentData
+}
+
+export function useAssignmentsByCourse(courseID: number) {
+  const { data } = useLiveQuery(db.select().from(assignments).where(eq(assignments.course_id, courseID)))
+  const assignmentData = data.map(convertRawAssignment)
+  return assignmentData
+}
+
+export function useAssignmentsByDay(date: Date) {
+  const { data } = useLiveQuery(db.select().from(assignments).where(
+    and(
+      gte(assignments.due, startOfDay(date).toISOString()),
+      lte(assignments.due, endOfDay(date).toISOString())
+    )
+  ), [date])
+  const assignmentData = data.map(convertRawAssignment)
+  return assignmentData
+}
+
+export function getAssignmentById(id: number | null) {
+  const data = useSQLiteContext().getFirstSync<rawAssignment>(`
+      SELECT 
+      id,
+      title,
+      course_id,
+      description,
+      due,
+      due_type,
+      completed_at
+      FROM assignments 
+      WHERE id = ${id}`)
+  if (data === null) return null
+  const assignment = convertRawAssignment(data)
+  return assignment// If id is null, data is null too
+}
+
+export async function toggleCompleted(id: number, completedAt: string | undefined, table: AnySQLiteTable) {
+  if (completedAt) {
+    await db.update(assignments).set({ completed_at: null }).where(eq(assignments.id, id))
+  }
+  else if (!completedAt) {
+    await db.update(assignments).set({ completed_at: new Date().toISOString() }).where(eq(assignments.id, id))
+  }
+};
+
+// Courses
+export function useCourses() {
+  const semesterID = useUserStore((state) => state.semesterID)
+  const { data } = useLiveQuery(db.select().from(courses).where(eq(courses.semester_id, Number(semesterID))), [semesterID])
+  const courseData = data.map(convertRawCourse)
+  return courseData
+}
+
+export function getCourseById(id: number | null) {
+  const data = useSQLiteContext().getFirstSync<Course>(`
+      SELECT 
+      code,
+      name,
+      color,
+      semester_id,
+      instructor
+      FROM courses 
+      WHERE id = ${id}
+      `)
+  return data // If id is null, data is null too
+}
+
+// Events
+export function useCurrentEvent() {
+  const { data } = useLiveQuery(db.select().from(events).where(
+    and(
+      lte(events.start_date, new Date().toISOString()),
+      gte(events.end_date, new Date().toISOString())
+    )
+  ))
+  const eventData = data.map(convertRawEvent)
+  return eventData
+}
+
+export function useEventsByDay(date: Date) {
+  const { data } = useLiveQuery(db.select().from(events).where(
+    and(
+      gte(events.start_date, startOfDay(date).toISOString()),
+      lte(events.end_date, endOfDay(date).toISOString())
+    )
+  ), [date])
+  const eventData = data.map(convertRawEvent)
+  return eventData
+}
+
+export function useEventsByDateRange(firstDay: Date, lastDay: Date) {
+  const { data } = useLiveQuery(db.select().from(events).where(
+    and(
+      gte(events.start_date, startOfDay(firstDay).toISOString()),
+      lte(events.end_date, endOfDay(lastDay).toISOString())
+    )
+  ))
+  const eventData = data.map(convertRawEvent)
+  return eventData
+}
+
+// Semesters
+export function useSemesters() {
+  const { data } = useLiveQuery(db.select().from(semesters))
+  const semesterData = data.map(convertRawSemester)
+  return semesterData
+}
+
+export function getSemesterById(id: number | null) {
+  const data = useSQLiteContext().getFirstSync<Semester>(`
+   SELECT
+   name,
+   start,
+   end
+   FROM semesters
+   WHERE id = ${id} 
+    `)
+  return data
+}
+
+// Tasks
 export function useTasks() {
   // const todayPattern = new Date().toISOString().slice(0, 10) + '%'; // timezone bug
   // const { data } = useLiveQuery(db.select().from(tasks).where(like(tasks.due, todayPattern)))
@@ -64,135 +201,3 @@ export function useTasksByDateRange(firstDay: Date, lastDay: Date) {
   const taskData = data.map(convertRawTask)
   return taskData
 }
-
-export function useCourses() {
-  const semesterID = useUserStore((state) => state.semesterID)
-  const { data } = useLiveQuery(db.select().from(courses).where(eq(courses.semester_id, Number(semesterID))), [semesterID])
-  const courseData = data.map(convertRawCourse)
-  return courseData
-}
-
-export function getCourseById(id: number | null) {
-  const data = useSQLiteContext().getFirstSync<Course>(`
-      SELECT 
-      code,
-      name,
-      color,
-      semester_id,
-      instructor
-      FROM courses 
-      WHERE id = ${id}
-      `)
-  return data // If id is null, data is null too
-}
-
-export function useSemesters() {
-  const { data } = useLiveQuery(db.select().from(semesters))
-  const semesterData = data.map(convertRawSemester)
-  return semesterData
-}
-
-export function getSemesterById(id: number | null) {
-  const data = useSQLiteContext().getFirstSync<Semester>(`
-   SELECT
-   name,
-   start,
-   end
-   FROM semesters
-   WHERE id = ${id} 
-    `)
-  return data
-}
-
-export function useCurrentEvent() {
-  const { data } = useLiveQuery(db.select().from(events).where(
-    and(
-      lte(events.start_date, new Date().toISOString()),
-      gte(events.end_date, new Date().toISOString())
-    )
-  ))
-  const eventData = data.map(convertRawEvent)
-  return eventData
-}
-
-export function useEventsByDay(date: Date) {
-  const { data } = useLiveQuery(db.select().from(events).where(
-    and(
-      gte(events.start_date, startOfDay(date).toISOString()),
-      lte(events.end_date, endOfDay(date).toISOString())
-    )
-  ), [date])
-  const eventData = data.map(convertRawEvent)
-  return eventData
-}
-
-export function useEventsByDateRange(firstDay: Date, lastDay: Date) {
-  const { data } = useLiveQuery(db.select().from(events).where(
-    and(
-      gte(events.start_date, startOfDay(firstDay).toISOString()),
-      lte(events.end_date, endOfDay(lastDay).toISOString())
-    )
-  ))
-  const eventData = data.map(convertRawEvent)
-  return eventData
-}
-
-export function useAssignmentsByDateRange(firstDay: Date, lastDay: Date) {
-  const { data } = useLiveQuery(db.select().from(assignments).where(
-    and(
-      gte(assignments.due, startOfDay(firstDay).toISOString()),
-      lte(assignments.due, endOfDay(lastDay).toISOString())
-    )
-  ))
-  const assignmentData = data.map(convertRawAssignment)
-  return assignmentData
-}
-
-export function useAssignments() {
-  const { data } = useLiveQuery(db.select().from(assignments))
-  const assignmentData = data.map(convertRawAssignment)
-  return assignmentData
-}
-
-export function useAssignmentsByCourse(courseID: number) {
-  const { data } = useLiveQuery(db.select().from(assignments).where(eq(assignments.course_id, courseID)))
-  const assignmentData = data.map(convertRawAssignment)
-  return assignmentData
-}
-
-export function useAssignmentsByDay(date: Date) {
-  const { data } = useLiveQuery(db.select().from(assignments).where(
-    and(
-      gte(assignments.due, startOfDay(date).toISOString()),
-      lte(assignments.due, endOfDay(date).toISOString())
-    )
-  ), [date])
-  const assignmentData = data.map(convertRawAssignment)
-  return assignmentData
-}
-
-export function getAssignmentById(id: number | null) {
-  const data = useSQLiteContext().getFirstSync<rawAssignment>(`
-      SELECT 
-      id,
-      title,
-      course_id,
-      description,
-      due,
-      due_type,
-      completed_at
-      FROM assignments 
-      WHERE id = ${id}`)
-  if (data === null) return null
-  const assignment = convertRawAssignment(data)
-  return assignment// If id is null, data is null too
-}
-
-export async function toggleCompleted(id: number, completedAt: string | undefined, table: AnySQLiteTable) {
-  if (completedAt) {
-    await db.update(assignments).set({ completed_at: null }).where(eq(assignments.id, id))
-  }
-  else if (!completedAt) {
-    await db.update(assignments).set({ completed_at: new Date().toISOString() }).where(eq(assignments.id, id))
-  }
-};
