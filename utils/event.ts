d396e55b34b6b4f273c217db049d07ad16d1a6ed
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-09-13 00:50:58 -0600

    fix(events.ts): add check to see if the currently selected day is today before returning This prevents recurrence events from displaying as having already happened even though they technically haven't.

--- utils/event.ts
+++ utils/event.ts
@@ -4,6 +4,7 @@ import { datetime, rrulestr } from "rrule";
 
 import { Event } from "@/types";
 import { pretty } from ".";
+import { useCalendarStore } from "@/stores/calendar";
 
 function getRecurredEndDate(startDate: Date, endDate: Date, recurredStartDate: Date) {
   const offset = endDate.getTime() - startDate.getTime()
@@ -19,7 +20,7 @@ export const checkCurrentEvent = (event: Event): boolean => {
 export const checkHasActiveRecurrence = (event: Event): boolean => {
   if (!event.recurring) return false
   const recurringStartDateArray = getOccurrencesOnDay(event.recurring, new Date())
-  if (!recurringStartDateArray[0]) return false // does this event have a recurrence that takes place today? // this line is sketchy come back to it
+  if (!recurringStartDateArray[0]) return false // CHECK: does this event have a recurrence that takes place today? // this line is sketchy come back to it
   const recurredStartDate = recurringStartDateArray[0]
   const recurredEndDate = getRecurredEndDate(event.startDate, event.endDate, recurredStartDate)
   const now = dayjs()
@@ -27,7 +28,7 @@ export const checkHasActiveRecurrence = (event: Event): boolean => {
 }
 
 export const getOccurrencesOnDay = (recurrenceString: string, date: Date): Date[] => {
-  const startOfDate = startOfDay(date) // remove this line and directly use date parameter?
+  const startOfDate = startOfDay(date) // CHECK: remove this line and directly use date parameter?
   const startOfDatetime = datetime(startOfDate.getUTCFullYear(), startOfDate.getUTCMonth() + 1, startOfDate.getUTCDate(), 0, 0, 0)
   const endOfDatetime = datetime(startOfDate.getUTCFullYear(), startOfDate.getUTCMonth() + 1, startOfDate.getUTCDate(), 23, 59, 59)
   const occurrences = rrulestr(recurrenceString).between(startOfDatetime, endOfDatetime, true)
@@ -36,11 +37,13 @@ export const getOccurrencesOnDay = (recurrenceString: string, date: Date): Date[
 
 export const checkEventWasEarlierToday = (startDate: Date, endDate: Date, recurrenceString: string | undefined): boolean => {
   const today = new Date()
+  const { currentDate } = useCalendarStore()
+  if (!isSameDay(currentDate, today)) return false // CHECK: adding this logic works but it isn't smooth
   if (!recurrenceString) {
     return isSameDay(startDate, today) && isBefore(endDate, Date.now())
   }
   else {
-    const recurredStartDateArray = recurrenceString ? getOccurrencesOnDay(recurrenceString, new Date()) : null
+    const recurredStartDateArray = recurrenceString ? getOccurrencesOnDay(recurrenceString, currentDate) : null
     if (recurredStartDateArray && recurredStartDateArray.length > 0) {
       const recurredStartDate = recurredStartDateArray[0]
       const recurredEndDate = getRecurredEndDate(startDate, endDate, recurredStartDate)
