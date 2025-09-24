6ccfd98de30ef3b05feafc701f00da063faa0d77
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-09-23 23:36:47 -0600

    internal: add drawer date picker to semester form

--- app/semester-form.tsx
+++ app/semester-form.tsx
@@ -85,11 +85,12 @@ export default function SemesterForm() {
 
   return (
     <BottomSheetModalProvider>
-      <View style={[styles.container, {}]}>
+      <View style={[styles.container, { backgroundColor: theme.primary }]}>
         <View style={[styles.formContainer, {}]}>
           <PrimaryTextInputField placeholder='Add semester name' value={name} onChangeText={setName} />
           {/* <DatePicker dateType='start' date={start} setDate={setStart} form='semester' />
           <DatePicker dateType='end' date={end} setDate={setEnd} form='semester' /> */}
+          <SemesterDatePicker />
           <SemesterDatePicker />
         </View>
         <ButtonRow create={createSemester} update={updateSemester} confirmDelete={confirmDelete} isCreateForm={id === undefined} disabled={!semesterInsertSchema.safeParse(semester).success} />
--- components/SemesterForm/SemesterDatePicker.tsx
+++ components/SemesterForm/SemesterDatePicker.tsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-
-import { useTheme } from '@/hooks/useTheme'
+import React, { useEffect, useState } from 'react'
 import { SymbolView } from 'expo-symbols'
 import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
-import { Button } from '@rneui/themed';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming, useAnimatedReaction } from 'react-native-reanimated';
+import { startOfDay } from 'date-fns';
+
+import { useTheme } from '@/hooks/useTheme'
+
+import PressableOpacity from '@/components/Buttons/PressableOpacity';
 
 type SemesterDatePickerProps = {
 
@@ -12,52 +15,125 @@ type SemesterDatePickerProps = {
 
 export default function SemesterDatePicker({ }: SemesterDatePickerProps) {
   const theme = useTheme()
-  const [date, setDate] = useState(new Date())
+  const [externalDate, setExternalDate] = useState<Date | null>(null)
+  const [internalDate, setInternalDate] = useState(new Date())
+
   const [showCalendar, setShowCalendar] = useState(false)
+  const height = useSharedValue(40)
+  const opacity = useSharedValue(0)
 
   const handlePickerChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
-    const currentDate = selectedDate || date;
-    setDate(currentDate)
+    if (selectedDate) setExternalDate(startOfDay(selectedDate))
+  };
+
+  useEffect(() => {
+    showCalendar ? height.value = 380 : height.value = 70
+    showCalendar ? opacity.value = 1 : opacity.value = 0
+  }, [showCalendar])
+
+  const rStyle = useAnimatedStyle(() => ({
+    height: withTiming(height.value)
+  }))
+
+  const qStyle = useAnimatedStyle(() => ({
+    // opacity: withTiming(opacity.value)
+    opacity: 1
+  }))
+
+  function handlePress() {
+    setShowCalendar(!showCalendar)
+    !externalDate && setExternalDate(startOfDay(new Date()))
+  }
+
+  function clearDate() {
+    setShowCalendar(false)
+    setExternalDate(null)
+  }
+
+  // useAnimatedReaction(
+  //   () => {
+  //     return opacity.value;
+  //   },
+  //   (currentValue, previousValue) => {
+  //     console.log(currentValue)
+  //   }
+  // );
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.field}>
-        <View style={styles.subContainer}>
+    <Animated.View style={[styles.container, { backgroundColor: theme.secondary }, rStyle]}>
+      <View style={styles.fieldContainer}>
+        <View style={styles.halfFieldContainer}>
           <SymbolView name="calendar" />
-          <Text>Start Date</Text>
+          <Text style={[styles.labelText, { color: theme.text }]}>Start Date</Text>
         </View>
-        <View style={styles.subContainer}>
-          <Button title='open' onPress={() => setShowCalendar(!showCalendar)} />
+        <View style={styles.halfFieldContainer}>
+          {/* <Button title='open' onPress={() => setShowCalendar(!showCalendar)} /> */}
+          <PressableOpacity style={[styles.button, { backgroundColor: theme.grey200 }]} onPress={handlePress}>
+            <Text style={[styles.dateText, { color: theme.text }]}>{externalDate === null ? 'No Date' : externalDate.toLocaleDateString(undefined, { year: 'numeric', month: 'short', day: 'numeric' })}</Text>
+          </PressableOpacity>
+          {
+            externalDate &&
+            <PressableOpacity style={[styles.clearButton, { backgroundColor: theme.grey200 }]} onPress={clearDate}>
+              <SymbolView name='xmark' size={15} />
+            </PressableOpacity>
+          }
         </View>
       </View>
-      {
-        showCalendar &&
+      <Animated.View style={[qStyle, styles.pickerContainer]}>
         <DateTimePicker
           testID="dtp"
-          value={date}
+          value={internalDate}
           mode={'date'}
           display={'inline'}
           onChange={handlePickerChange}
-        // accentColor={theme.accent}
         />
-      }
-    </View>
+      </Animated.View>
+    </Animated.View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'space-between',
-    backgroundColor: 'grey'
+    overflow: 'hidden',
+    // paddingTop: 10,
+    paddingHorizontal: 40,
+    paddingTop: 15,
+    borderRadius: 10,
+    // alignItems: 'center',
+    // backgroundColor: '#03045e'
   },
-  field: {
+  fieldContainer: {
+    // alignSelf: 'stretch',
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    // backgroundColor: '#971ecfff'
   },
-  subContainer: {
+  halfFieldContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 10
-  }
+    gap: 10,
+    // backgroundColor: '#00b4d8'
+  },
+  pickerContainer: {
+
+  },
+  labelText: {
+    fontSize: 16,
+  },
+  dateText: {
+
+  },
+  button: {
+    padding: 10,
+    borderRadius: 8,
+  },
+  clearButton: {
+    padding: 10,
+    borderRadius: 20,
+  },
 })
